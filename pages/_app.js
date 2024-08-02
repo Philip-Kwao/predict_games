13574b8850efa76c2c51c23f14e3ab1503f1bc65
Philip Kwao <kwaoizy@gmail.com>
2024-08-02 17:19:31 +0000

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { Roboto, Poppins, Noto_Sans, Quantico } from 'next/font/google'
 
+const quantico=Quantico({
+  subsets:["latin"],
+  weight:['400','700']
+})
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <main className={quantico.className} >
+      <Component {...pageProps} />
+    </main>
+  )
 }
